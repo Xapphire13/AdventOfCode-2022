813450babab85f4a9ad256f8bf9c2e43aa9c5c49
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2022-12-03 01:07:09 -0800

    refactor code

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,68 +1,61 @@
-fn get_shape_score(shape: &String) -> u32 {
-    match shape.as_str() {
-        "ROCK" => 1,
-        "PAPER" => 2,
-        "SCISSORS" => 3,
-        _ => panic!("Invalid shape"),
-    }
-}
-
-fn get_opponent_shape(opponent_play: &String) -> &str {
-    match opponent_play.as_str() {
-        "A" => "ROCK",
-        "B" => "PAPER",
-        "C" => "SCISSORS",
-        _ => panic!("Invalid play"),
-    }
-}
-
-fn get_my_shape(my_play: &String) -> &str {
-    match my_play.as_str() {
-        "X" => "ROCK",
-        "Y" => "PAPER",
-        "Z" => "SCISSORS",
-        _ => panic!("Invalid play"),
-    }
-}
-
 pub fn run(input: Vec<String>) {
     println!("Part 1: {}", part1(&input));
 }
 
-fn part1(input: &Vec<String>) -> u32 {
-    let find_winner = |p1: &String, p2: &String| {
-        let p1_val = get_shape_score(p1);
-        let p2_val = get_shape_score(p2);
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
 
-        if ((p1_val % 3) + 1) == p2_val {
-            return 1;
-        } else if ((p2_val % 3) + 1) == p1_val {
-            return -1;
-        }
+fn get_shape_score(shape: Shape) -> u32 {
+    match shape {
+        Shape::Rock => 1,
+        Shape::Paper => 2,
+        Shape::Scissors => 3,
+    }
+}
 
-        return 0;
+fn get_opponent_shape(opponent_play: &str) -> Shape {
+    match opponent_play {
+        "A" => Shape::Rock,
+        "B" => Shape::Paper,
+        "C" => Shape::Scissors,
+        _ => panic!("Invalid play!"),
+    }
+}
+
+fn get_my_shape(my_play: &str) -> Shape {
+    match my_play {
+        "X" => Shape::Rock,
+        "Y" => Shape::Paper,
+        "Z" => Shape::Scissors,
+        _ => panic!("Invalid play!"),
+    }
+}
+
+fn play(opponent_shape: Shape, my_shape: Shape) -> u32 {
+    let opponent_shape_val = get_shape_score(opponent_shape);
+    let my_shape_val = get_shape_score(my_shape);
+    let result = if ((opponent_shape_val % 3) + 1) == my_shape_val {
+        6 // We win
+    } else if ((my_shape_val % 3) + 1) == opponent_shape_val {
+        0 // We lose
+    } else {
+        3 // Tie
     };
 
+    return result + my_shape_val;
+}
+
+fn part1(input: &Vec<String>) -> u32 {
     let mut score = 0;
 
     for line in input {
         let split: [&str; 2] = line.split(" ").collect::<Vec<&str>>().try_into().unwrap();
         match split {
             [opponent_play, my_play] => {
-                let op = opponent_play.to_string();
-                let mp = my_play.to_string();
-                let opponent_shape = get_opponent_shape(&op);
-                let my_shape = get_my_shape(&mp);
-                let winner = find_winner(&opponent_shape.into(), &my_shape.into());
-
-                let win_score = match winner {
-                    0 => 3,
-                    1 => 6,
-                    _ => 0,
-                };
-
-                score += win_score;
-                score += get_shape_score(&my_shape.to_string());
+                score += play(get_opponent_shape(opponent_play), get_my_shape(my_play))
             }
         }
     }
